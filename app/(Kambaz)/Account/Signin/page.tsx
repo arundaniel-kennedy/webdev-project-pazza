9e65c2f1bdcfa878c45326d49aff470099ee1385
Arun Daniel <arundanielkennedy@gmail.com>
2025-12-07 20:33:21 -0500

    added info to sigin page

--- app/(Kambaz)/Account/Signin/page.tsx
+++ app/(Kambaz)/Account/Signin/page.tsx
@@ -7,6 +7,7 @@ import { useState } from "react";
 import { FormControl, Button } from "react-bootstrap";
 
 import * as client from "../client";
+import "./page.scss";
 
 export default function Signin() {
   const [credentials, setCredentials] = useState({
@@ -18,7 +19,7 @@ export default function Signin() {
     e.preventDefault();
     const user = await client.signin(credentials);
     if (user.hasOwnProperty("message")) {
-      alert(user.message)
+      alert(user.message);
     } else {
       if (!user) return;
       dispatch(setCurrentUser(user));
@@ -27,39 +28,72 @@ export default function Signin() {
   };
 
   return (
-    <div id="wd-signin-screen" style={{ width: "350px" }}>
-      <h2>Sign in</h2>
-      <FormControl
-        id="wd-email"
-        placeholder="email"
-        className="mb-2"
-        defaultValue={credentials.username}
-        onChange={(e) =>
-          setCredentials({ ...credentials, username: e.target.value })
-        }
-      />
-      <FormControl
-        id="wd-password"
-        placeholder="password"
-        type="password"
-        className="mb-2"
-        defaultValue={credentials.password}
-        onChange={(e) =>
-          setCredentials({ ...credentials, password: e.target.value })
-        }
-      />
-      <Button
-        type="button"
-        onClick={signin}
-        id="wd-signin-btn"
-        className="btn btn-primary w-100 mb-2"
-      >
-        Sign in
-      </Button>
-      <br />
-      <Link href="Signup" id="wd-signup-link">
-        Sign up
-      </Link>
+    <div
+      id="wd-signin-screen"
+      // style={{ width: "350px" }}
+    >
+      <div className="content-left">
+        <h2>Sign in</h2>
+        <FormControl
+          id="wd-email"
+          placeholder="email"
+          className="mb-2"
+          defaultValue={credentials.username}
+          onChange={(e) =>
+            setCredentials({ ...credentials, username: e.target.value })
+          }
+        />
+        <FormControl
+          id="wd-password"
+          placeholder="password"
+          type="password"
+          className="mb-2"
+          defaultValue={credentials.password}
+          onChange={(e) =>
+            setCredentials({ ...credentials, password: e.target.value })
+          }
+        />
+        <Button
+          type="button"
+          onClick={signin}
+          id="wd-signin-btn"
+          className="btn btn-primary w-100 mb-2"
+        >
+          Sign in
+        </Button>
+        <br />
+        <Link href="Signup" id="wd-signup-link">
+          Sign up
+        </Link>
+      </div>
+      <div className="content-right">
+        <h2>Project Details</h2>
+        <h4>Kambaz Pazza</h4>
+        Project Participants:
+        <ul>
+          <li>Arun Daniel Kenendy - Section 04</li>
+          <li>Arth Sindekar - Section 04</li>
+          <li>Ashwin Gurumurthy - Section 04</li>
+        </ul>
+        <div>
+          <b>Github Frontend URL:</b>
+          <a
+            target="_blank"
+            href="https://github.com/arundaniel-kennedy/webdev-project-pazza"
+          >
+            https://github.com/arundaniel-kennedy/webdev-project-pazza
+          </a>
+        </div>
+        <div className="mt-2">
+          <b>Github Backend URL:</b>
+          <a
+            target="_blank"
+            href="https://github.com/arundaniel-kennedy/webdev-project-pazza-backend"
+          >
+            https://github.com/arundaniel-kennedy/webdev-project-pazza-backend
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
