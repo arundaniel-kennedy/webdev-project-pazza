de4268a92d5c3083d529494370b82f5638c639d0
Arun Daniel <arundanielkennedy@gmail.com>
2025-12-12 09:46:46 -0500

    fixes

--- app/(Kambaz)/Courses/[cid]/Pazza/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Pazza/page.tsx
@@ -1,5 +1,13 @@
+"use client";
+
+import { useParams } from "next/navigation";
+
 export default function page() {
+    const { cid } = useParams();
     return (
-        <iframe src="/Pazza" style={{ width: "100%", height: "calc(100vh - 110px)"}}></iframe>
-    )
-};
+        <iframe
+            src={`/Pazza/Class/${cid}`}
+            style={{ width: "100%", height: "calc(100vh - 110px)" }}
+        ></iframe>
+    );
+}
